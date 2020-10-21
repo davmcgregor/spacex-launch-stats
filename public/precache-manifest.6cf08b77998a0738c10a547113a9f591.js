af9fe127d06d71da91a130ddeab625594d7d30e0
David McGregor <davidjohnmcgregor@gmail.com>
2020-10-21 23:12:38 +0100

    Prepare for deploy

---
+++ public/precache-manifest.6cf08b77998a0738c10a547113a9f591.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2e922f0d5ba31f02ed9211bef16d19a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2b0f46487c5253a010a",
+    "url": "/static/css/main.d67066ea.chunk.css"
+  },
+  {
+    "revision": "f0dbd460d02a8aa7cf69",
+    "url": "/static/js/2.8d495275.chunk.js"
+  },
+  {
+    "revision": "d5c6ebb23b0d90d5369d2408e52e8b9d",
+    "url": "/static/js/2.8d495275.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2b0f46487c5253a010a",
+    "url": "/static/js/main.c8d59d0f.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "ab10d000cdf7ee45a57395feee160ad2",
+    "url": "/static/media/logo.ab10d000.png"
+  }
+]);
